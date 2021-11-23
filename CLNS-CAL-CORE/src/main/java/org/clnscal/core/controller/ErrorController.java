0d6b33fb8070882bdfebae586ee00a9ef77d7279
georgy.saukov <gsaukov@yahoo.com>
2021-11-24 00:01:07 +0100

    error handling.

--- CLNS-CAL-CORE/src/main/java/org/clnscal/core/controller/ErrorController.java
+++ CLNS-CAL-CORE/src/main/java/org/clnscal/core/controller/ErrorController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
 
 import javax.naming.AuthenticationException;
@@ -35,15 +34,17 @@ public class ErrorController {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler({MissingServletRequestParameterException.class})
     public String handleValidationExceptions(
-            MissingServletRequestParameterException ex) {
-        return ex.getMessage();
+            MissingServletRequestParameterException e) {
+        LOG.warn("Resource not found ", e.getMessage());
+        return e.getMessage();
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler({WebClientResponseException.class})
     public String handleWebClientException(
-            WebClientResponseException ex) {
-        return ex.getMessage();
+            WebClientResponseException e) {
+        LOG.warn("Downstream failure ", e);
+        return e.getMessage();
     }
 
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
